e6601447ecf6383ece995d023d30ef323fb59241
shinyglasses <crunchytvtable@gmail.com>
2024-10-21 09:21:56 -0400

    Create characters.py

---
+++ characters.py
@@ -0,0 +1,37 @@
+class Enemy:
+    def __init__(self, name, maxhp, currenthp, strength, weapon, golddrop, weapondrop):
+        self.name = name
+        self.maxhp = maxhp
+        self.currenthp = currenthp
+        self.strength = strength
+        self.weapon = weapon
+        self.golddrop = golddrop
+        self.weapon
+
+class Enemy:
+    def __init__(self, name, maxhp, currenthp, )
+
+"""Player Character:
+‘id’ : 1
+‘name’ : “playerperson”
+‘maxhp’: 75
+‘currenthp’: 75 (will change)
+‘strength’: 5
+‘gold’: 0 (will change)
+‘inventory’: [list of dictionaries with different items]
+sample item dictionary, all the items are weapons to simplify things
+
+‘Name’: wooden sword
+‘damage’: 10
+
+Enemy Characters:
+
+‘id’ : 2
+‘name’ : “slime”
+‘maxhp’: 20
+‘currenthp’: 20 (will change)
+‘strength’: 5
+‘golddrop’: 10
+‘weapondrop’ : “nothing”
+
+"""
