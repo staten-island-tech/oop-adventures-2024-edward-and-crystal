6c187afbb1201d0eb9ca52551dddafcb0f44e15b
shinyglasses <crunchytvtable@gmail.com>
2024-10-25 09:20:42 -0400

    Create ui.py

---
+++ ui.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+window = tk.Tk()
+window.geometry('1800x900')
+window.title('SUPER AWESOME AND VERY REAL GAME by Edward and Crystal!!')
+label = tk.Label(window, text='test test testt')
+window.mainloop()
