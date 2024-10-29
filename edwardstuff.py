8ec5a10e5435533ac77f394009502c1702ce4fc0
Edward-Mercado <edwardmercado149@gmail.com>
2024-10-29 12:22:13 -0400

    bruh

--- edwardstuff.py
+++ edwardstuff.py
@@ -1,12 +1,8 @@
-import tkinter as tk
-from tkinter import font
 
-window = tk.Tk()
-window.title('SUPER AWESOME AND VERY REAL GAME by Edward and Crystal!!')
-window.geometry('400x400')
 
-textfont = font.Font(family='Comic Sans MS', size=100, weight='normal')
 
-textbox = tk.Text(window, width=400, height=400)
-textbox.pack(side=tk.BOTTOM, pady= 400)
-window.mainloop()
+
+
+
+
+
