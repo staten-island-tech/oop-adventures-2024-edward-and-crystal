9d954d560a3437dcea12da60788a56701e401ffd
Edward-Mercado <edwardmercado149@gmail.com>
2024-11-08 08:56:54 -0500

    asdf

--- menus.py
+++ menus.py
@@ -7,13 +7,16 @@ class Menu():
     def SelectItem(item, menuvar):
         menuvar.set(item)
 
-    def SelectConfirm(window, item):
+    def Confirm(window, item, prompt):
         global confirmbuttons
         global ask
         menuvar = tk.StringVar()
         for button in menubuttons:
             button.destroy()
-        ask = tk.Label(window, text=f'Would you like to select the {item.name}?')
+        if prompt == 'select':
+            ask = tk.Label(window, text=f'Would you like to select the {item.name}?')
+        elif prompt == 'end':
+            ask = tk.Label(window, text='Would you like to close the menu?')
         yes_button = tk.Button(window, text="Yes", command=lambda: Menu.SelectItem('YES', menuvar))
         no_button = tk.Button(window, text="No", command=lambda: Menu.SelectItem("NO", menuvar))
         confirmbuttons = [yes_button, no_button]
@@ -47,7 +50,7 @@ class Menu():
         finish = False
         while finish == False:
             item = Menu.Inventory1(window, player)
-            confirm = Menu.SelectConfirm(window, item)
+            confirm = Menu.Confirm(window, item, 'select')
             if confirm == "YES":
                 if isinstance(item, Weapon):
                     player.weapon = item
@@ -56,18 +59,44 @@ class Menu():
                         button.destroy()
                         ask.destroy()
                     outcome.pack()
-                    finish = True
+                    confirm = Menu.Confirm(window, item, 'end')
+                    if confirm == 'YES':
+                        finish = True
+                        for button in confirmbuttons:
+                            button.destroy()
+                        outcome.destroy()
+                        ask.destroy()
+                        break
+                    elif confirm == 'NO':
+                        for button in confirmbuttons:
+                            button.destroy()
+                        outcome.destroy()
+                        ask.destroy()
                 if isinstance(item, HealingItem):
-                    player.PlayerHeal(item.heal)
+                    item.UseHealingItem(player)
+                    player.inventory.remove(item)
                     outcome = tk.Label(window, text=f'You consumed the {item.name}!')
                     outcome2 = tk.Label(window, text=f'You now have {player.currenthp} HP!')
-
+                    outcome.pack()
+                    outcome2.pack()
                     for button in confirmbuttons:
                         button.destroy()
                         ask.destroy()
-                    outcome.pack()
-                    outcome2.pack()
-                    finish = True
+                    confirm = Menu.Confirm(window, item, 'end')
+                    if confirm == 'YES':
+                        finish = True
+                        for button in confirmbuttons:
+                            button.destroy()
+                        outcome.destroy()
+                        outcome2.destroy()
+                        ask.destroy()
+                        break
+                    elif confirm == 'NO':
+                        for button in confirmbuttons:
+                            button.destroy()
+                        outcome.destroy()
+                        outcome2.destroy()
+                        ask.destroy()
             elif confirm == 'NO':
                 for button in confirmbuttons:
                     button.destroy()
@@ -82,8 +111,9 @@ window.title("Menu")
 weapon = Weapon('weapon', 0, 10, 10)
 apple = HealingItem('apple', 10, 10)
 healingpot = HealingItem('healing potion', 10, 10)
+goblinsword = Weapon('goblin sword', 100, 10, 1000)
 
-player = MainCharacter('name', 10, 10, 10, weapon, [weapon, apple], 0, 0, 0)
+player = MainCharacter('name', 10, 10, 10, weapon, [weapon, apple, healingpot, goblinsword], 0, 0, 0)
 
 Menu.Inventory(window, player)
 window.mainloop()
