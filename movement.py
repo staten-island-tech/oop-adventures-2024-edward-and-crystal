88ea75215039c319685268dfe568fbe713d3d5af
Edward-Mercado <edwardmercado149@gmail.com>
2024-12-01 14:52:10 -0500

    starting to work on the movement for real

--- movement.py
+++ movement.py
@@ -9,9 +9,25 @@ class OpenWorld():
     def LoadRoom(room):
         while running:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT():
+                if event.type == pygame.QUIT:
                     quit()
-            screen.fill((10, 10, 10))
+            
+            screen.fill((20, 20, 25))
+            for rect in room['rectangles']:
+                pygame.draw.rect(screen, (40, 40, 50), rect)
+                
+            pygame.draw.rect(screen, (185, 220, 240), (0, 620, 1280, 100))
+            
+            pygame.display.update()
             
     def Movement(player):
         pass
+    
+room = {
+    "rectangles" : [
+    (100, 100, 100, 400),
+    (200, 200, 400, 50)]
+    }   
+    
+OpenWorld.LoadRoom(room)
+
