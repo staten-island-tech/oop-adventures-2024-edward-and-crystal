31f171f731aaca91f87a4af408b0c9390e965db9
Edward-Mercado <edwardmercado149@gmail.com>
2024-12-02 17:59:53 -0500

    from the screen

--- battles.py
+++ battles.py
@@ -3,6 +3,7 @@ from charactersitems import MainCharacter
 from charactersitems import Enemy
 from charactersitems import BossEnemy
 from charactersitems import Grifter
+from charactersitems import Weapon
 import pygame
 pygame.init()
 
@@ -13,9 +14,13 @@ running = True
 
 class Battles:
     def DrawActionButton(player, enemies, action, events):
-        global wasactiondone
+        global wasactiondone, attackpressed
+        menufont = pygame.font.Font(None, 30)
+        actiontext = menufont.render(action, True, (255, 255, 255))
         if action == 'ATTACK':
             buttonrect = pygame.Rect((220, 635, 150, 50))
+            if attackpressed == True:
+                actiontext = menufont.render(action, True, (215, 230, 255))
         elif action == 'BLOCK':
             buttonrect = pygame.Rect((380, 635, 150, 50))
         elif action == 'FLEE (coward)':
@@ -27,8 +32,6 @@ class Battles:
             color = (25, 25, 32.5)
         
         pygame.draw.rect(screen, color, buttonrect)
-        menufont = pygame.font.Font(None, 30)
-        actiontext = menufont.render(action, True, (255, 255, 255))
         actionsurface = actiontext.get_rect(center=buttonrect.center)
         screen.blit(actiontext, actionsurface)
         
@@ -38,11 +41,50 @@ class Battles:
                     Battles.FleeCoward(player)
                     wasactiondone = True
                 elif action == 'ATTACK':
-                    Battles.Attack(player, enemies)
-                    wasactiondone = True
+                    if attackpressed == True:
+                        attackpressed = False
+                    else:
+                        attackpressed = True
                 else:
                     Battles.Block(player, enemies)
                     wasactiondone = True
+        
+        if attackpressed == True:
+            xcoordinate = 0
+            events = pygame.event.get()
+            for enemy in enemies:
+                ycoordinate = 60
+                if isinstance(enemy, BossEnemy):
+                    xcoordinate += 30
+                    ycoordinate = 0
+                attackenemy = pygame.Rect(xcoordinate+15, 20+ycoordinate, 120, 30)
+                font = pygame.font.Font(None, 20)
+                if attackenemy.collidepoint(pygame.mouse.get_pos()):
+                    color = (220, 220, 250)
+                    text = font.render("ATTACK THIS", True, (0, 0, 0))
+                else:
+                    color = (200, 200, 220)
+                    text = font.render("ATTACK THIS", True, (20, 20, 35))
+                pygame.draw.rect(screen, color, attackenemy)
+                textsurface = text.get_rect(center=attackenemy.center)
+                screen.blit(text, textsurface)
+            
+                xcoordinate += 150
+                if isinstance(enemy, BossEnemy):
+                    xcoordinate += 30
+                for event in events:
+                    if event.type == pygame.MOUSEBUTTONDOWN and attackenemy.collidepoint(pygame.mouse.get_pos()):
+                        attackpressed = False
+                        wasactiondone = True
+                        Battles.AttackStepTwo(player, enemy, enemies)
+                        
+    def AttackStepTwo(player, enemy, enemies):
+        damage = Character.CharacterDamageCalc(player)
+        enemy.currenthp -= damage
+        if enemy.currenthp < 0:
+            enemy.currenthp = 0
+            enemies.remove(enemy)
+        
                 
     def FleeCoward(player):
         global battle
@@ -54,9 +96,9 @@ class Battles:
         areyarunning = random.randint(1, 100)
         if areyarunning >= chancetoflee:
             battle = False
+        else:
+            print('fail lolol')
             
-    def Attack(player, enemies):
-        print(player.name)
     # make a makeshift for loop that waits for confirm buttons to be pressed
     
     def Block(player, enemies):
@@ -75,6 +117,16 @@ class Battles:
                 pygame.draw.line(screen, (200, 90, 75), (xcoordinate+115, 400), (xcoordinate+115, 425), 30)
                 pygame.draw.line(screen, (200, 90, 75), (xcoordinate+75, 225), (xcoordinate+35, 340), 27)
                 pygame.draw.line(screen, (200, 90, 75), (xcoordinate+75, 225), (xcoordinate+115, 340), 27)
+                
+                healthbarrect = pygame.Rect(xcoordinate+15, 530, 120, 30)
+                pygame.draw.rect(screen, (100, 130, 140), healthbarrect)
+                pygame.draw.rect(screen, (150, 195, 240), (xcoordinate+15, 530, (enemy.currenthp / enemy.maxhp) * 120, 30))
+                
+                font = pygame.font.Font(None, 36)
+                healthbartext = font.render(f"{enemy.currenthp} / {enemy.maxhp} HP", True, (255, 255, 255))
+                healthbarsurface = healthbartext.get_rect(center=healthbarrect.center)
+                screen.blit(healthbartext, healthbarsurface)
+                
             elif isinstance(enemy, BossEnemy):
                 xcoordinate += 30
                 pygame.draw.circle(screen, (150, 50, 55), (xcoordinate+75, 150), 70)
@@ -85,8 +137,17 @@ class Battles:
                 pygame.draw.line(screen, (150, 50, 55), (xcoordinate+115, 405), (xcoordinate+115, 435), 47)
                 pygame.draw.line(screen, (150, 50, 55), (xcoordinate+75, 210), (xcoordinate+25, 360), 42)
                 pygame.draw.line(screen, (150, 50, 55), (xcoordinate+75, 210), (xcoordinate+125, 360), 42)
-            
-            # make this its own function 
+
+                healthbarrect = pygame.Rect(xcoordinate-15, 515, 180, 60)
+                pygame.draw.rect(screen, (100, 130, 140), healthbarrect)
+                pygame.draw.rect(screen, (150, 215, 240), (xcoordinate-15, 515, (enemy.currenthp / enemy.maxhp) * 180, 60))
+                
+                font = pygame.font.Font(None, 46)
+                healthbartext = font.render(f"{enemy.currenthp} / {enemy.maxhp} HP", True, (255, 255, 255))
+                healthbarsurface = healthbartext.get_rect(center=healthbarrect.center)
+                screen.blit(healthbartext, healthbarsurface)
+                
+            # make this its own function / nah ima do my own thing
             nametagrect = pygame.Rect(xcoordinate+15, 450, 120, 40)
             listname = len((enemy.name))
             if listname < 7:
@@ -106,9 +167,10 @@ class Battles:
                 xcoordinate +=30
     
     def BattleMenu(player, enemies):
-        global wasactiondone, running
+        global wasactiondone, running, attackpressed
         running = False
         wasactiondone = False
+        attackpressed = False
         while battle:
             screen.fill((20, 20, 25))
             events = pygame.event.get()
@@ -147,12 +209,13 @@ class Battles:
                 Battles.DrawActionButton(player, enemies, "BLOCK", events)
                 Battles.DrawActionButton(player, enemies, "FLEE (coward)", events)
             
+            
             Battles.MakeEnemies(enemies)
             
             pygame.display.update()      
                 
-player = MainCharacter('edward', 100, 90, 20, 'nothing', [], 100, 10, 1) 
-goblin = Enemy('GoblinAAA', 1, 1, 1, 1, 1, 1, 1)
+player = MainCharacter('edward', 100, 90, 20, Weapon('supersword', 1, 1, 1), [], 100, 10, 1) 
+goblin = Enemy('GoblinAAA', 10, 1, 1, 1, 1, 1, 1)
 goblina = Enemy('Goblin', 1, 1, 1, 1, 1, 1, 1)
 goblinb = BossEnemy('GoblinAAAAAAAAAA', 1, 1, 1, 1, 1)
 goblinc = Enemy('Goblin', 1, 1, 1, 1, 1, 1, 1)
