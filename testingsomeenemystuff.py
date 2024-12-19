ce61f4aab26400c3d4b5a93af6b4ead64af65adf
Edward-Mercado <edwardmercado149@gmail.com>
2024-12-19 09:12:52 -0500

    asdf

--- testingsomeenemystuff.py
+++ testingsomeenemystuff.py
@@ -1,6 +1,165 @@
 import json
 
-enemies = [{'name': 'Goblin', 'type': 'REGULAR', 'maxhp': 10, 'currenthp': 10, 'strength': 5, 'weapon': {'name': 'Goblin Sword', 'strength': 0, 'durability': 9000, 'cost': 0}, 'goldrop': 5, 'weapondrop': {'name': 'Goblin Sword', 'type': 'WEAPON', 'strength': 5, 'durability': 14, 'cost': 0}, 'expdrop': 10}, {'name': 'Orc', 'type': 'REGULAR', 'maxhp': 18, 'currenthp': 18, 'strength': 9, 'weapon': {'name': 'Club', 'strength': 0, 'durability': 9000, 'cost': 0}, 'goldrop': 5, 'weapondrop': {'name': 'Strong Club', 'type': 'WEAPON', 'strength': 9, 'durability': 8, 'cost': 0}, 'expdrop': 19}, {'name': 'Slime', 'type': 'REGULAR', 'maxhp': 7, 'currenthp': 7, 'strength': 4, 'weapon': {'name': 'Goo', 'strength': 0, 'durability': 9000, 'cost': 0}, 'goldrop': 5, 'weapondrop': 'nothing', 'expdrop': 5}, {'name': 'Strong Slime', 'type': 'REGULAR', 'maxhp': 15, 'currenthp': 15, 'strength': 5, 'weapon': {'name': 'Slime Secretion', 'strength': 0, 'durability': 9000, 'cost': 0}, 'goldrop': 5, 'weapondrop': {'name': 'Slime Secretion', 'type': 'HEALING ITEM', 'heal': 10, 'cost': 0}, 'expdrop': 16}, {'name': 'Strong Orc', 'type': 'REGULAR', 'maxhp': 35, 'currenthp': 35, 'strength': 15, 'weapon': {'name': 'Orc Club', 'strength': 0, 'durability': 9000, 'cost': 0}, 'goldrop': 5, 'weapondrop': {'name': 'Orc Club', 'type': 'WEAPON', 'strength': 17, 'durability': 13, 'cost': 0}, 'expdrop': 34}, {'name': 'Grifter', 'type': 'GRIFTER', 'maxhp': 20, 'currenthp': 20, 'steal': 15, 'weapon': {'name': 'Grifter Staff', 'strength': 0, 'durability': 9000, 'cost': 0}, 'goldrop': 5, 'weapondrop': {'name': 'Grifter Staff', 'strength': 20, 'durability': 3, 'cost': 0}, 'expdrop': 27}, {'name': 'Boss', 'type': 'BOSS', 'maxhp': 140, 'currenthp': 140, 'strength': 30, 'weapon': {'name': 'Stick', 'strength': 3, 'durability': 10000000000000000000000000000000000000000000000000000000000, 'cost': 0}, 'expdrop': 500}, {'name': 'Boss Grifter', 'type': 'GRIFTER', 'maxhp': 200, 'currenthp': 200, 'steal': 10, 'weapon': {'name': 'Grifter Staff', 'strength': 0, 'durability': 9000, 'cost': 0}, 'goldrop': 5, 'weapondrop': {'name': 'Grifter Staff', 'strength': 135, 'durability': 1, 'cost': 0}, 'expdrop': 69}]
-
+enemies = [
+    {
+        "type" : "REGULAR",
+        "name" : "Goblin",
+        "maxhp" : 12,
+        "currenthp" : 12,
+        "strength" : 5,
+        "golddrop" : 3,
+        "expdrop" : 7,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        },
+        "weapondrop" : {
+            "type" : "WEAPON",
+            "name" : "Goblin Club",
+            "strength" : 6,
+            "durability" : 10,
+            "cost" : 6
+        }
+    },
+    {
+        "type" : "REGULAR",
+        "name" : "Orc",
+        "maxhp" : 17,
+        "currenthp" : 17,
+        "strength" : 9,
+        "golddrop" : 6,
+        "expdrop" : 13,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        },
+        "weapondrop" : {
+            "type" : "WEAPON",
+            "name" : "Orc Club",
+            "strength" : 10,
+            "durability" : 17,
+            "cost" : 10
+        }
+    },
+    {
+        "type" : "REGULAR",
+        "name" : "Slime",
+        "maxhp" : 10,
+        "currenthp" : 10,
+        "strength" : 4,
+        "golddrop" : 5,
+        "expdrop" : 6,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        },
+        "weapondrop" : {
+            "type" : "HEALING ITEM",
+            "name" : "Secretion",
+            "heal" : 6,
+            "cost" : 4
+        }
+    },
+    {
+        "type" : "REGULAR",
+        "name" : "Strong Orc",
+        "maxhp" : 23,
+        "currenthp" : 23,
+        "strength" : 15,
+        "golddrop" : 18,
+        "expdrop" : 17,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        },
+        "weapondrop" : {
+            "type" : "WEAPON",
+            "name" : "Gold Orc Club",
+            "strength" : 23,
+            "durability" : 20,
+            "cost" : 18
+        }
+    },
+    {
+        "type" : "REGULAR",
+        "name" : "Strong Slime",
+        "maxhp" : 15,
+        "currenthp" : 15,
+        "strength" : 8,
+        "golddrop" : 10,
+        "expdrop" : 9,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        },
+        "weapondrop" : {
+            "type" : "HEALING ITEM",
+            "name" : "Cool Slime Secretion",
+            "heal" : 14,
+            "cost" : 10
+        }
+    },
+    {
+        "type" : "GRIFTER",
+        "name" : "Grifter",
+        "maxhp" : 24,
+        "currenthp" : 24,
+        "strength" : 0,
+        "steal" : 7,
+        "golddrop" : 15,
+        "expdrop" : 17,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        },
+        "weapondrop" : {
+            "type" : "WEAPON",
+            "name" : "Grifter Staff",
+            "strength" : 8,
+            "durability" : 1000,
+            "cost" : 19
+        }
+    },
+    {
+        "type" : "GRIFTER",
+        "name" : "Boss Grifter",
+        "maxhp" : 70,
+        "currenthp" : 70,
+        "strength" : 0,
+        "steal" : 12,
+        "golddrop" : 37,
+        "expdrop" : 29,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        },
+        "weapondrop" : {
+            "type" : "HEALING ITEM",
+            "name" : "Grifter's Treasure",
+            "heal" : 50,
+            "cost" : 60
+        }
+    },
+    {
+        "type" : "BOSS",
+        "name" : "Boss Grifter",
+        "maxhp" : 140,
+        "currenthp" : 140,
+        "strength" : 20,
+        "expdrop" : 39,
+        "weapon" : {
+            "name" : "none",
+            "strength" : 0,
+            "durability" : 8192
+        }
+    }
+]
 with open('enemies.json', 'w') as file:
-    json.dump(enemies, file, indent=4)
+    json.dump(enemies, file, indent=2)
