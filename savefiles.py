8c34828770ecefe5618e7029746f3de415fdc94b
shinyglasses <crunchytvtable@gmail.com>
2025-01-04 21:02:48 -0500

    wow....it's so nice when you can FINALLY code because bugs dont stop you 😄

--- savefiles.py
+++ savefiles.py
@@ -9,47 +9,84 @@ try:
 except(FileNotFoundError, json.JSONDecodeError):
 	savedata = []
 
-def make_savefile(name):
-	
-	blank_player_data = {
-		"name": name,
-		"maxhp": 100,
-		"currenthp": 100,
-		"strength": 0,
-		"weapon": "wooden_sword", 
-		"inventory": [],
-		"gold": 0,
-		"level": 1,
-		"exp": 0
-	}
-	savedata.append(blank_player_data)
-    
-    # Write the updated save data back to the file
-	with open('saves.json', 'w') as file:
-		json.dump(savedata, file, indent=2)
-''
-def find_savefile():
-	lookingforsave = True
+class SaveFileManager:
+	def __init__(self):
+		pass #done so I can call this class's functions in other functions also in the class
 
-	while lookingforsave:
-		savefound = False
-		confirmed = False
+	def save_or_load_file(self):
+		#strip removes all spaces before or after the letters (not the spaces in between words)
+		answer = input('Do you want to create a file or load a new one?').strip().lower()
 
-	name = input('Whose save would you like to open?')
+		if answer == 'create':
+			name = input('What do you want to name your save file?').strip()
+			self.make_savefile(name)
+		elif answer == 'load':
+			self.select_savefile()
+		else:
+			print('Invalid Answer. Please try again!')
+			self.save_or_load_file() #forces them to answer load or save 
 
-	for savefile in savedata:
-		if savefile['name'].upper() == name.upper():
-				chosensave = savefile
-				savefound = True
-		if savefound == True:
-			print()
-			# print all the data of this save im too lazy to type it out
-			confirm = input('Is this the save you would like to open? Y/N')
-			if confirm.upper() == 'Y':
-				lookingforsave = False
+	def make_savefile(self, name):
+		
+		blank_player_data = {
+			"name": name,
+			"maxhp": 100,
+			"currenthp": 100,
+			"strength": 0,
+			"weapon": "wooden_sword", 
+			"inventory": ['wooden_sword'],
+			"gold": 0,
+			"level": 1,
+			"exp": 0
+		}
 
-	#then you set the player values, coordinates, and room number equal to the values in the save file in the place where you call this function
+		savedata.append(blank_player_data)
+		
+		# update the json file with the new blank player data
+		with open('saves.json', 'w') as file:
+			json.dump(savedata, file, indent=2)
+	''
+	def select_savefile(self):
 
-	
-	
-find_savefile()
+		lookingforsave = True
+
+		while lookingforsave:
+			savefound = False
+
+			print('\nHere are all the save file names:\n') #\n skips a line. done for better formatting
+
+			#prints all savefile names
+			for savefile in savedata: 
+				print(savefile['name'])
+			
+			name = input('\nWhich save would you like to open?').upper().strip() 
+
+			for savefile in savedata:
+				if savefile['name'].upper() == name:
+					chosensave = savefile
+					savefound = True
+				else:
+					print('\nInvalid Answer. Please try again.')
+					self.select_savefile()
+						
+				if savefound == True:
+					if savefile == chosensave:
+
+						#for better formatting. format: Key: pair, with each key value pair on a new line. EX: Weapon: wooden_sword
+						for key, value in chosensave.items():
+						
+							#converts the inventory (a list) into a string to remove the brackets and quotes when printing, aka to make it look better
+							if isinstance(value, list):  
+								value = ", ".join(value)  
+							print(f'{key.capitalize()}: {value}')
+
+					confirm = input('Is this the save you would like to open? Y/N')
+					if confirm.upper() == 'Y':
+						lookingforsave = False
+						return chosensave
+		#when you call this function, you set the player values, coordinates, and room number equal to the values in the save file 
+		
+		
+test = SaveFileManager()
+
+test.save_or_load_file()
