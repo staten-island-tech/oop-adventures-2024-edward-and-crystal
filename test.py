26ed4467ae2235ef117156c821f8401119348956
Edward-Mercado <edwardmercado149@gmail.com>
2025-01-10 16:57:00 -0500

    bruh

--- test.py
+++ test.py
@@ -2,9 +2,7 @@ import json
 with open('saves.json', 'r') as file:
   saves = json.load(file)
   
-saves.clear()
+saves.append('test')
 
 with open('saves.json', 'w') as file:
   json.dump(saves, file)
-  
-print(saves)
